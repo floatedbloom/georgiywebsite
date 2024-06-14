a391e7856b9de158f06d4c3c2e784dcabff46136
floatedbloom <georgelapin23@gmail.com>
2024-06-13 21:24:14 -0700

    fdszgv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});{
-    build: {
-      rollupOptions: {
-        external: ['path/to/chartjs/dist/chart.umd.js']
-      }
-    }
-  }
+export default defineConfig({});
